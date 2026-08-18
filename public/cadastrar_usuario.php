f4da6903a153223eb584b0394124535212afb77b
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-08-18 12:25:26 -0300

    terminei cadastrar usuario

--- public/cadastrar_usuario.php
+++ public/cadastrar_usuario.php
@@ -6,3 +6,14 @@ $nome = $_POST["nome_usuario"];
 $email = $_POST["email"];
 
 $sql = "INSERT INTO usuarios (nome,email) VALUES (?,?)";
+
+$stmt=mysqli_prepare($conexao, $sql);
+
+if($stmt){
+    mysqli_stmt_bind_param($stmt, "ss", $nome, $email);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+}
+header("Location: ../index.php");
+exit();
+?>
