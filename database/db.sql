d783a3721f1ac6c2d6f2c6b1a760466a688786a6
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-08-18 09:31:14 -0300

    add - arrumei um pouco do index e fiz o banco de dados, fiz a conexão e o css

---
+++ database/db.sql
@@ -0,0 +1,18 @@
+
+CREATE DATABASE pratos_Marcos_m4;
+USE pratos_Marcos_m4;
+
+CREATE TABLE Pratos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10, 2) NOT NULL,
+    usuario_id INT NOT NULL
+);
+    CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR (50) NOT NULL,
+    email VARCHAR (100) NOT NULL
+    
+    FOREIGN KEY usuario_id REFERENCES usuarios(id)
+);
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+include "infra/conexao.php";
+$livros = mysqli_query($conexao, "SELECT * FROM Pratos");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade_Pratos</title>
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>Atividade_Pratos</h1>
+    </header>
+    <main>
+        <h2>Adicione um novo Prato!</h2>
+        <form action="public/cadastrar.php" method="POST">
+            <label for="prato">Nome do prato:</label>
+            <input type="text" name="prato">
+            <br>
+            <label for="Descri">Descrição:</label>
+            <input type="text" name="descri">
+            <br>
+            <label for="categoria">Categoria:</label>
+            <input type="text" name="categoria">
+            <br>
+            <label for="preco">Preço:</label>
+            <input type="number" name="preco">
+            <br>
+            <button type="submit">Cadastrar</button>
+        </form>
+        <div>
+            <h2>Pratos Cadastrados</h2>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Categoria</th>
+                    <th>Preço</th>
+                    <th>Ações</th>
+                </tr>
+                <?php while ($Pratos = mysqli_fetch_assoc($Pratos)) { ?>
+                    <tr>
+                        <td><?php echo $Pratos["id"] ?></td>
+                        <td><?php echo $Pratos["nome"] ?></td>
+                        <td><?php echo $Pratos["descri"] ?></td>
+                        <td><?php echo $Pratos["categoria"] ?></td>
+                        <td><?php echo $Pratos["preco"] ?></td>
+                        <td>
+                            <a href="public/editar.php?id=<?php echo $livro["id"] ?>">Editar</a>
+                            <a href="public/excluir.php?id=<?php echo $livro["id"] ?>">Excluir</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+
+    </main>
+    <footer>
+
+    </footer>
+
+
+</body>
+
+</html>
