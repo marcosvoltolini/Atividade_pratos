9c15c5673feff8541d09714064a8de46c90987e6
davigura <davi_gura@estudante.sesisenai.org.br>
2026-08-19 08:21:06 -0300

    terminei o listar

---
+++ public/listar.php
@@ -0,0 +1,54 @@
+<?php
+require "conexao.php";
+
+
+$sql = "SELECT pratos.id, pratos.nome, pratos.descricao, pratos.preco, usuarios.nome AS nome_usuario
+        FROM pratos
+        INNER JOIN usuarios ON pratos.usuario_id = usuarios.id";
+
+$resultado = mysqli_query($conexao, $sql);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Pratos</title>
+</head>
+<body>
+
+<h1>Lista de Pratos</h1>
+
+<a href="cadastrar_prato.php">Cadastrar novo prato</a>
+<br><br>
+
+<table border="1" cellpadding="8">
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Descrição</th>
+        <th>Preço</th>
+        <th>Cadastrado por</th>
+        <th>Ações</th>
+    </tr>
+
+    <?php while ($linha = mysqli_fetch_assoc($resultado)) { ?>
+        <tr>
+            <td><?php echo $linha["id"]; ?></td>
+            <td><?php echo $linha["nome"]; ?></td>
+            <td><?php echo $linha["descricao"]; ?></td>
+            <td>R$ <?php echo number_format($linha["preco"], 2, ",", "."); ?></td>
+            <td><?php echo $linha["nome_usuario"]; ?></td>
+            <td>
+                <a href="editar_prato.php?id=<?php echo $linha["id"]; ?>">Editar</a>
+                |
+                <a href="excluir_prato.php?id=<?php echo $linha["id"]; ?>"
+                   onclick="return confirm('Tem certeza que deseja excluir?')">Excluir</a>
+            </td>
+        </tr>
+    <?php } ?>
+
+</table>
+
+</body>
+</html>
