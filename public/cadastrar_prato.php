0f36deb33b38d389e3098120f37f0fb973fc5e5e
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-08-19 07:54:52 -0300

    add - fiz grande parte do cadastrar prato, só não terminei pq o vscode está bugado

---
+++ public/cadastrar_prato.php
@@ -0,0 +1,43 @@
+<?php
+include "../infra/conexao.php";
+
+$nome = $_POST["nome_prato"];
+$descricao = $_POST["descri"];
+$categoria = $_POST["categoria"];
+$preco = $_POST["preco"];
+$nome_usuario = $_POST["nome_usuario"];
+
+$sql = "SELECT id FROM usuarios WHERE nome= ?";
+
+$stmt = mysqli_prepare($conexao,$sql);
+
+mysqli_stmt_bind_param($stmt,"s", $nome_usuario);
+
+mysqli_stmt_execute($stmt);
+
+$resultado = mysqli_stmt_get_result($stmt);
+
+$usuario = mysqli_fetch_assoc($resultado);
+
+mysqli_stmt_close($stmt);
+
+if (!$usuario) {
+    die("Usuário não encontrado.");
+}
+$usuario_id = $usuario["id"];
+
+$sql = "INSERT INTO Pratos (nome, descricao,preco,categoria, usuario_id)VALUES (?, ?, ?, ? ,?)";
+
+$stmt = mysqli_prepare ($conexao, $sql);
+
+if ($stmt) {
+    mysqli_stmt_bind_param(
+        $stmt,
+        "ssdsi",
+        $nome,
+        $descricao,
+        $categoria,
+        $preco,
+        
+    )
+}
